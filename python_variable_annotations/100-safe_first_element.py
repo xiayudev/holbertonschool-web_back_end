6f4858b0a778161d067986eab5c380e8afd8525e
xiayudev <jctuesta7@gmail.com>
2023-08-28 20:32:25 -0500

    feat: function return either Any or None
    Signed-off-by: xiayudev <jctuesta7@gmail.com>

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module to work with typing
+"""
+
+
+# The types of the elements of the input are not know
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
