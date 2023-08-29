336f93a9611418f95f513d21d98b71bbe3e195c9
xiayudev <jctuesta7@gmail.com>
2023-08-28 21:14:23 -0500

    fix: add return type
    Signed-off-by: xiayudev <jctuesta7@gmail.com>

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -5,8 +5,11 @@
 
 from typing import Any, Mapping, TypeVar, Union
 
+T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), None] = None):
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """Return a value from a key or value by default"""
     if key in dct:
         return dct[key]
