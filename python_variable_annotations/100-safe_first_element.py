46683abe9f5b29d0eaed1a0bd24cb5a42df6f095
xiayudev <jctuesta7@gmail.com>
2023-08-28 20:33:27 -0500

    fix (docs): add docs for function
    Signed-off-by: xiayudev <jctuesta7@gmail.com>

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -8,6 +8,7 @@ from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return first element of a sequence or None"""
     if lst:
         return lst[0]
     else:
