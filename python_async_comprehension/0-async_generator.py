c87afe69e3894e153488c2057b38e73ee3e8905c
xiayudev <jctuesta7@gmail.com>
2023-08-30 20:50:08 -0500

    fix: return type for generator
    Signed-off-by: xiayudev <jctuesta7@gmail.com>

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
