8ec525864045c383f6fec2f3eb95d9e5a364cd9d
xiayudev <jctuesta7@gmail.com>
2023-09-06 12:24:59 -0500

    fix: use typing Tuple
    Signed-off-by: xiayudev <jctuesta7@gmail.com>

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Module to work with pagination
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return index start and index end"""
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    pagination = (start_index, end_index)
-    return pagination
+    return start_index, end_index
