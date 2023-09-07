0fee35d65af0332eead9ac5e6f3cdcd3be2e7873
xiayudev <jctuesta7@gmail.com>
2023-09-06 23:17:45 -0500

    feat: delete-resilient pagination
    Signed-off-by: xiayudev <jctuesta7@gmail.com>

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import Dict, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """return a dictionary with some values"""
+        assert index < page_size and index < len(self.dataset())
+        data = []
+        iterator = index
+        while True:
+            if len(data) == page_size or iterator > page_size + index:
+                break
+            if self.indexed_dataset().get(iterator):
+                data.append(self.indexed_dataset().get(iterator))
+            iterator += 1
+        return {
+            'index': index,
+            'next_index': iterator,
+            'page_size': page_size,
+            'data': data
+        }
