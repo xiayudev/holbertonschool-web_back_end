dbdeb4b9c866ac3f9902c6ced36ed0d569f3251d
xiayudev <jctuesta7@gmail.com>
2023-09-17 11:25:55 -0500

    feat: implement a class currency

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() { return `${this._name} (${this._code})`; }
+
+  get code() { return this._code; }
+
+  set code(code) { this._code = code; }
+
+  get name() { return this._name; }
+
+  set name(name) { this._name = name; }
+}
