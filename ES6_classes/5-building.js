52ecfde8908a444183bd4936bbdd858429b1693a
xiayudev <jctuesta7@gmail.com>
2023-09-17 13:06:52 -0500

    feat: override a method

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() { return this._sqft; }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
