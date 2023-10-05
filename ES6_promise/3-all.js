9a8f98142e384a808459c240966ff881d690d68c
xiayudev <jctuesta7@gmail.com>
2023-10-05 09:51:03 -0500

    fix: correct handle of errors inside then

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,10 @@
 import * as utils from './utils';
 
-export default async function handleProfileSignup() {
-  const response = await Promise.all([utils.uploadPhoto().catch(() => console.log('Signup system offline')),
-    utils.createUser().catch(() => console.log('Signup system offline'))]);
-  const upload = response[0];
-  const create = response[1];
-  console.log(`${upload.body} ${create.firstName} ${create.lastName}`);
+export default function handleProfileSignup() {
+  return Promise.all([utils.uploadPhoto(), utils.createUser()])
+    .then((response) => {
+      const upload = response[0];
+      const create = response[1];
+      console.log(`${upload.body} ${create.firstName} ${create.lastName}`);
+    }, () => console.log('Signup system offline'));
 }
