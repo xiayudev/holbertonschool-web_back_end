3dd30b92dbb9802825fe3aed817a5b243bf470a5
xiayudev <jctuesta7@gmail.com>
2023-10-05 11:47:01 -0500

    fix: correct handle of errors

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,16 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([uploadPhoto(fileName).catch((err) => err),
+  return Promise.allSettled([uploadPhoto(fileName).catch((err) => err),
     signUpUser(firstName, lastName).catch((err) => err)])
-    .then((response) => [
-      {
-        status: 'rejected',
-        value: response[0] || response[0].message,
-      },
-      {
-        status: 'fulfilled',
-        value: response[1] || response[1].message,
-      },
-    ]);
+    .then((response) => {
+      const data = [...response];
+      data[0].value = `Error: ${data[0].value.message}`;
+    });
 }
