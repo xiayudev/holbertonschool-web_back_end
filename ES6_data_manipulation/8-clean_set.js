d57641be4f483ce526e8c21bfd9d7faafdc06b3c
xiayudev <jctuesta7@gmail.com>
2023-10-05 21:09:29 -0500

    fix: use startsWith instead of includes

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   const allString = [];
   if (startString.length > 0) {
     set.forEach((string) => {
-      if (string.includes(`${startString}`)) {
+      if (string.startsWith(`${startString}`)) {
         allString.push(string.trim());
       }
     });
