e62f51edd3bfc6e1f1d7f448acd50ac9560e0eb9
xiayudev <jctuesta7@gmail.com>
2023-10-05 21:29:11 -0500

    fix: return empty string when no string is passed

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const allString = [];
-  if (startString) {
+  if (startString && typeof startString === 'string') {
     set.forEach((string) => {
       if (string && string.startsWith(`${startString}`)) {
         allString.push(string.substring(startString.length));
