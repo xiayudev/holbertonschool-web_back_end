a15180544068cc8bcc62858a61b295d1868f12a9
xiayudev <jctuesta7@gmail.com>
2023-10-05 21:25:12 -0500

    fix: add condition if string is undefined

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   const allString = [];
   if (startString) {
     set.forEach((string) => {
-      if (string.startsWith(`${startString}`)) {
+      if (string && string.startsWith(`${startString}`)) {
         allString.push(string.substring(startString.length));
       }
     });
