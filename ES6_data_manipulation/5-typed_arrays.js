e815fa43ed47d84eb9bda81eb327bf21cc667154
xiayudev <jctuesta7@gmail.com>
2023-10-05 19:37:48 -0500

    feat: create a typed array

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(lenght, position, value) {
+  if (position < 0 || position > lenght) {
+    throw Error('Position outside range');
+  }
+  const myBuffer = new ArrayBuffer(lenght);
+  const myInt8 = new DataView(myBuffer);
+  myInt8[position] = value;
+  return myBuffer;
+}
